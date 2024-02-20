1c111e1cba0eeca5525f5d3e8e23756deed3ef27
Martín Fernández de Retana <martin.f@opendeusto.es>
2024-02-20 19:41:50 +0100

    He aprendido a leer T-T  (ahora el texteditor lee un archivo por path)(aun no guarda)

--- OpenGLGraphics/Core/Editor/Editor.cpp
+++ OpenGLGraphics/Core/Editor/Editor.cpp
@@ -8,6 +8,7 @@
 #include "Interface/TextEditor.h"
 
 Editor::Editor() : editorLocked(false) {}
+bool firstTime = true;
 
 
 bool Editor::IsEditorLocked() {
@@ -27,24 +28,24 @@ void Editor::Render(Core::Graphics::OpenGLPipeline& pipeline) {
     assetManager.Render();
     properties.Render();
     outliner.Render();
-    textEditor.SetHandleKeyboardInputs(true);
-    char text[100] = "patata";
-    ImGui::Begin("Text Editor");
 
-    textEditor.SetReadOnly(false);
-    //textEditor.SetText(text);
+    ImGui::Begin("Text Editor");
     ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
     textEditor.Render("Patata", ImVec2{ viewportPanelSize.x, viewportPanelSize.y }, false);
-    static const char* fileToEdit = "patata.txt";
-    //	static const char* fileToEdit = "test.cpp";
-
+    static const char* fileToEdit = "Core/Editor/patata.txt";
+    { // Save
+		if (ImGui::Button("Save")) {
+			std::ofstream t(fileToEdit);
+			t << textEditor.GetText();
+		}
+	}
+    std::ifstream t(fileToEdit);
+    //std::cout << "Error: " << strerror(errno) << std::endl;
+    if (t.good() & firstTime)
     {
-        std::ifstream t(fileToEdit);
-        if (t.good())
-        {
-            std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
-            textEditor.SetText(str);
-        }
+        std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+        textEditor.SetText(str);
+        firstTime = false;
     }
     ImGui::End();
     
@@ -54,4 +55,8 @@ void Editor::Render(Core::Graphics::OpenGLPipeline& pipeline) {
     uint64_t textureID = pipeline.GetRenderFrameBuffer()->GetTextureHandle();
     ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{ viewportPanelSize.x, viewportPanelSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
     ImGui::End();
+}
+
+void editamos() {
+	std::cout << "Editamos" << std::endl;
 }
