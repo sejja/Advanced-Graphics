a990322a2f8a1086ca201dc5604f7545e569e2a8
None3075 <martin.f@opendeusto.es>
2024-02-20 11:56:42 +0100

    Edito el texto de la bibliayihhjhj

--- OpenGLGraphics/Core/Editor/Editor.cpp
+++ OpenGLGraphics/Core/Editor/Editor.cpp
@@ -1,5 +1,8 @@
 #include "Editor.h"
 #include <iostream>
+#include <fstream>
+#include <iterator>
+#include <string>
 #include "Interface/Properties.h"
 #include "Interface/AssetManager.h"
 #include "Interface/TextEditor.h"
@@ -11,7 +14,7 @@ bool Editor::IsEditorLocked() {
     return editorLocked;
 }
 
-void Editor::Render(Core::Graphics::OpenGLPipeline& pipeline){
+void Editor::Render(Core::Graphics::OpenGLPipeline& pipeline) {
 
     //If any input is being used, camera controls are locked
     editorLocked = ImGui::IsAnyItemActive();
@@ -25,14 +28,25 @@ void Editor::Render(Core::Graphics::OpenGLPipeline& pipeline){
     properties.Render();
     outliner.Render();
     textEditor.SetHandleKeyboardInputs(true);
+    char text[100] = "patata";
     ImGui::Begin("Text Editor");
 
     textEditor.SetReadOnly(false);
-    textEditor.SetText("Tonto el que lo lea");
+    //textEditor.SetText(text);
     ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
     textEditor.Render("Patata", ImVec2{ viewportPanelSize.x, viewportPanelSize.y }, false);
+    static const char* fileToEdit = "patata.txt";
+    //	static const char* fileToEdit = "test.cpp";
+
+    {
+        std::ifstream t(fileToEdit);
+        if (t.good())
+        {
+            std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+            textEditor.SetText(str);
+        }
+    }
     ImGui::End();
-    
     
     ImGui::Begin("Scene");
     viewportPanelSize = ImGui::GetContentRegionAvail();
