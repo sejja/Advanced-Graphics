14ec88fecb2af299dcf9a563147f6cc592b8d7a6
Diego Revilla <diego.revilla.r@gmail.com>
2024-02-20 14:33:36 +0100

    Starts support for GLTF Models

---
+++ OpenGLGraphics/Graphics/Primitives/GLBModel.cpp
@@ -0,0 +1,15 @@
+﻿//
+//	GLBModel.cpp
+//	OpenGL Graphics
+//
+//	Created by Diego Revilla on 20/02/23
+//	Copyright � 2023. All Rights reserved
+//
+
+#include "GLBModel.h"
+
+namespace Graphics {
+	namespace Primitives {
+
+	}
+}
---
+++ OpenGLGraphics/Graphics/Primitives/GLBModel.h
@@ -0,0 +1,18 @@
+﻿//
+//	GLBModel.cpp
+//	OpenGL Graphics
+//
+//	Created by Diego Revilla on 20/02/23
+//	Copyright � 2023. All Rights reserved
+//
+
+#ifndef _GRAPHIC_LANGUAGE_BINARY_MODEL__H_
+#define _GRAPHIC_LANGUAGE_BINARY_MODEL__H_
+
+namespace Graphics {
+	namespace Primitives {
+
+	}
+}
+
+#endif
