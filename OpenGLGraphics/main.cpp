46fb5f8947eb84d97ac5209c5fab7a8777886ded
Lingfeng555 <121032780+Lingfeng555@users.noreply.github.com>
2024-03-11 11:24:44 +0100

    no he hecho nada pero comiteo para molestar

--- OpenGLGraphics/main.cpp
+++ OpenGLGraphics/main.cpp
@@ -24,15 +24,6 @@ int main() {
     
 	app.mScene.CreateScene("Content/Maps/Scene.level",  app.GetPipelineRef() , [&app](const std::shared_ptr<Core::Object>& obj) {
         obj->ForEachComponent([&app](const std::shared_ptr<Core::Component>& comp) {
-
-            /*
-            std::shared_ptr<Core::Particles::ParticleMangager> particleManager = std::dynamic_pointer_cast<Core::Particles::ParticleMangager>(comp);
-            if (particleManager)
-            {
-                app.GetPipelineRef().SetParticleManager(std::dynamic_pointer_cast<Core::Particles::ParticleMangager>(comp));
-                std::cout << "Se ha anadido ParticlesManager\n";
-            }
-            */
             std::shared_ptr<Core::Graphics::Renderable> renderable = std::dynamic_pointer_cast<Core::Graphics::Renderable>(comp);
             //If the object is a renderable
             if (renderable) app.GetPipelineRef().AddRenderable(renderable);
