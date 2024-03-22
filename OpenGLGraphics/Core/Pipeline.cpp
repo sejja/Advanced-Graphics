63a46adbf19c97440a63e40f5b0c7981c5371a22
Diego Revilla <diego.revilla.r@gmail.com>
2024-03-22 14:42:23 +0100

    Grouped Renderables as vectors, instead of queues

--- OpenGLGraphics/Core/Pipeline.cpp
+++ OpenGLGraphics/Core/Pipeline.cpp
@@ -1,5 +1,13 @@
+//
+//	Pipeline.cpp
+//	OpenGL Graphics
+//
+//	Created by Diego Revilla on 22/03/24
+//	Copyright © 2024. All Rights reserved
+//
+
 #include "Pipeline.h"
 
-void Core::Pipeline::AddRenderable(std::weak_ptr<Renderable> x) {
-	mGroupedRenderables.emplace(x.lock());
+void Core::Pipeline::AddRenderable(const std::weak_ptr<Renderable>& x) {
+	mGroupedRenderables.emplace_back(x.lock());
 }
--- OpenGLGraphics/Core/Pipeline.h
+++ OpenGLGraphics/Core/Pipeline.h
@@ -2,14 +2,14 @@
 //	Pipeline.h
 //	OpenGL Graphics
 //
-//	Created by Diego Revilla on 10/03/22
-//	Copyright © 2023. All Rights reserved
+//	Created by Diego Revilla on 22/03/24
+//	Copyright © 2024. All Rights reserved
 //
 
 #ifndef _PIPELINE__H_
 #define _PIPELINE__H_
 
-#include <queue>
+#include <vector>
 #include <memory>
 #include "glm.hpp"
 
@@ -31,9 +31,9 @@ namespace Core {
 		virtual void PostRender() = 0;
 		virtual void Shutdown() = 0;
 		virtual void SetDimensions(const glm::lowp_u16vec2& dim) = 0;
-		void AddRenderable(std::weak_ptr<Renderable> x);
+		void AddRenderable(const std::weak_ptr<Renderable>& x);
 	protected:
-		std::priority_queue<std::shared_ptr<Renderable>> mGroupedRenderables;
+		std::vector<std::shared_ptr<Renderable>> mGroupedRenderables;
 	};
 }
 
