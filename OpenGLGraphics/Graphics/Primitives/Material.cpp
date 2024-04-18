1ac4487cf34f56a0afc3e0c68ddf4dd14a681404
Diego Revilla <diego.revilla.r@gmail.com>
2024-04-18 20:52:56 +0200

    Material Definition Changed slightly

--- OpenGLGraphics/Graphics/Primitives/Material.cpp
+++ OpenGLGraphics/Graphics/Primitives/Material.cpp
@@ -2,12 +2,11 @@
 //	Material.cpp
 //	OpenGL Graphics
 //
-//	Created by Diego Revilla on 09/08/23
-//	Copyright © 2023. All Rights reserved
+//	Created by Diego Revilla on 18/04/24
+//	Copyright © 2024. All Rights reserved
 //
 
 #include "Material.h"
-#include <exception>
 
 namespace Graphics {
     namespace Primitives {
@@ -18,7 +17,7 @@ namespace Graphics {
         */ // ---------------------------------------------------------------------
         void Material::SetShininess(const float n) {
             if (n < 0.f || n > 1.f)
-                throw std::exception("n is not in the range of 0 to 1");
+                throw MaterialException("n is not in the range of 0 to 1");
 
             mShininess = n;
         }
