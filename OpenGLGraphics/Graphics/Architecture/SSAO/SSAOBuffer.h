04beea25a15e03bb84857bd6925de94b6ad4c2b0
Diego Revilla <diego.revilla.r@gmail.com>
2024-04-18 19:47:12 +0200

    Ambient Oclussion Buffer

---
+++ OpenGLGraphics/Graphics/Architecture/SSAO/SSAOBuffer.h
@@ -0,0 +1,29 @@
+﻿//
+//	SSAOBuffer.h
+//	OpenGL Graphics
+//
+//	Created by Diego Revilla on 18/04/24
+//	Copyright � 2024 . All Rights reserved
+//
+
+#ifndef _SSAO_BUFFER__H_
+#define _SSAO_BUFFER__H_
+
+#include <glew.h>
+#include <glm.hpp>
+
+namespace Graphics {
+	namespace Architecture {
+		namespace SSAO {
+			class SSAOBuffer {
+			public:
+				SSAOBuffer(const glm::u16vec2& dim);
+
+			private:
+				GLuint mHandle, mColorBuffer;
+			};
+		}
+	}
+}
+
+#endif
